831842c5b4d4c13eb413dfc71bb895df132343b5
vysnoo <vishnukumaresan21@gmail.com>
2025-07-19 13:28:30 +0530

    MaxOfTwo

---
+++ src/day/one/assignment/MaxOfTwo.java
@@ -0,0 +1,13 @@
+package day.one.assignment;
+
+public class MaxOfTwo {
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 40;
+
+        int max = (num1 > num2) ? num1 : num2;
+
+        System.out.println("The maximum number is: " + max);
+    }
+}
