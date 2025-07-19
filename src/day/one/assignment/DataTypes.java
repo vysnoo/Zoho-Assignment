6da90006b372a4dd8d52860679c4d962e3dd99fd
vysnoo <vishnukumaresan21@gmail.com>
2025-07-19 13:22:41 +0530

    DataTypes

---
+++ src/day/one/assignment/DataTypes.java
@@ -0,0 +1,19 @@
+package day.one.assignment;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int myInt = 100;
+        double myDouble = 123.456;
+        float myFloat = 12.34f;
+        long myLong = 123456789L;
+        char myChar = 'A';
+        byte myByte = 127;
+
+        System.out.println("Integer value: " + myInt);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Character value: " + myChar);
+        System.out.println("Byte value: " + myByte);
+    }
+}
