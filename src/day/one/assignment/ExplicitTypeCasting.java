41ab324c46bf52e53317b97e6bb2025e710203fe
vysnoo <vishnukumaresan21@gmail.com>
2025-07-20 11:26:09 +0530

    ExplicitTypeCasting

---
+++ src/day/one/assignment/ExplicitTypeCasting.java
@@ -0,0 +1,9 @@
+package day.one.assignment;
+
+public class ExplicitTypeCasting {
+    public static void main(String[] args) {
+        double d= 10.34;
+        int n= (int) d;
+        System.out.println(n);
+    }
+}
