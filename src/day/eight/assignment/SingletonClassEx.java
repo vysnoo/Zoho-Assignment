2b1557c98268cc20615f0efae939ecb07e46dba8
vysnoo <vishnukumaresan21@gmail.com>
2025-08-13 10:36:39 +0530

    Singleton Class Ex

---
+++ src/day/eight/assignment/SingletonClassEx.java
@@ -0,0 +1,22 @@
+package day.eight.assignment;
+
+public class SingletonClassEx {
+
+    private static final SingletonClassEx obj  = new SingletonClassEx();
+
+    private SingletonClassEx(){
+    }
+
+    public static SingletonClassEx getInstance(){
+        return obj;
+    }
+}
+
+class SingletonMain{
+    public static void main(String[] args) {
+        SingletonClassEx obj1 = SingletonClassEx.getInstance();
+        SingletonClassEx obj2 = SingletonClassEx.getInstance();
+
+        System.out.println(obj1==obj2);
+    }
+}
